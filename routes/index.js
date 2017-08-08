d77c72e81bedd430c4ac71fc754bb65a37d7ae46
Maciej Sitko <maciej@neatcube.co.uk>
2017-08-08 23:32:07 +0100

    Inbefore parsin form data

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ const Recipes = require('../models/recipes');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
     const { count, rows: recipes } = await Recipes.findAndCountAll({ limit: 10 });
-    res.render('index', { recipes, count });
+    const cookingTimes = await Recipes.findAll({ group: ['cookingTime']});
+    res.render('index', { recipes, count, cookingTimes });
 });
 
 router.get('/:recipe', async (req, res, next) => {
     const recipe = await Recipes.findById(req.params.recipe);
-    console.log(JSON.stringify(recipe))
     res.render('recipe', { recipe });
 });
 
